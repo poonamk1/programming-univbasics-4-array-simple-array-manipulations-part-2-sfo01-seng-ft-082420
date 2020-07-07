babb0df366434d22bece4a07514c8d8bd614f930
punam kwatra <punam2005@hotmail.com>
2020-07-07 20:33:46 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+def using_concat(my_favorite_things, more_favs)
+ 
+  my_favorite_things.concat(more_favs)
+  
+  
+end
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  list_of_esoteric_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_esoteric_language = "Python"
+  new_array = list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
+end
+def using_uniq (haircuts)
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  new_array = haircuts.uniq 
+end
+ def using_flatten (instruments)
+  instruments.flatten
+   
+ end
+ def using_delete ( array,string)
+   array.delete(string)
+   array
+   
+  end
+  def using_delete_at (array,integer)
+    array.delete_at (integer)
+    
+ end
+  
+   
+   
+   
+   
+   
+   
+   
+ 
+   
+
+  
+   
+   
+   
+   
+   
